7f4a60c171f31b835dd60d18171e31bad72cfd87
hciguenas <hciguenas@pucp.pe>
2016-11-28 17:52:42 +0100

    New User Registration Layout

--- TravelApp/app/src/main/java/upc/eet/pma/travelapp/NewUserActivity.java
+++ TravelApp/app/src/main/java/upc/eet/pma/travelapp/NewUserActivity.java
@@ -3,16 +3,22 @@ package upc.eet.pma.travelapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class NewUserActivity extends AppCompatActivity {
 
     private Button mNewUserBtn;
+    private EditText mName;
+    private EditText mEmail;
+    private EditText mCreatePassword;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
 
         mNewUserBtn = (Button) findViewById(R.id.newuserBtn);
+        mName = (EditText) findViewById(R.id.nameTxt);
+        mEmail = (EditText) findViewById(R.id.emailTxt);
+        mCreatePassword = (EditText) findViewById(R.id.passwordtxt);
     }
 }
