cd5105b941814acaa9c4a22477428edb18e6a4d5
bertacumellas <bertacume@gmail.com>
2016-12-27 16:35:26 +0100

    New Java Class: RequestsActivity and SearchActivity

---
+++ TravelApp/app/src/main/java/upc/eet/pma/travelapp/RequestsActivity.java
@@ -0,0 +1,13 @@
+package upc.eet.pma.travelapp;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RequestsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requests);
+    }
+}
---
+++ TravelApp/app/src/main/java/upc/eet/pma/travelapp/SearchActivity.java
@@ -0,0 +1,12 @@
+package upc.eet.pma.travelapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
