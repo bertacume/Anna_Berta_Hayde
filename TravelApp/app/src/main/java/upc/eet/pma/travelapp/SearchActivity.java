27879d86a430c5019ede113ab11fe8a80cac1a53
bertacumellas <bertacume@gmail.com>
2016-12-30 18:03:09 +0100

    Display user email in the ListView

--- TravelApp/app/src/main/java/upc/eet/pma/travelapp/SearchActivity.java
+++ TravelApp/app/src/main/java/upc/eet/pma/travelapp/SearchActivity.java
@@ -1,36 +1,61 @@
 package upc.eet.pma.travelapp;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ListView;
+import android.widget.TextView;
 
+
+import com.firebase.client.Firebase;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity  {
 
-    ArrayAdapter<String> adapter;
+    FirebaseDatabase usersDatabase;
+    DatabaseReference usersDatabaseReference;
+    ArrayList userList;
+
+    ArrayAdapter adapter;
+    private  ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        ListView lv = (ListView)findViewById(R.id.listViewSearch);
-        ArrayList<String> array_t = new ArrayList<>();
-        array_t.addAll(Arrays.asList(getResources().getStringArray(R.array.array_try)));
 
-        adapter = new ArrayAdapter<>(
-                SearchActivity.this,
-                android.R.layout.simple_list_item_1,
-                array_t);
-        lv.setAdapter(adapter);
+        mListView = (ListView) findViewById(R.id.listViewSearch);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        usersDatabase = FirebaseDatabase.getInstance();
+        usersDatabaseReference = usersDatabase.getReference("Users");
+        addValueEventListener(usersDatabaseReference);
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,4 +81,31 @@ public class SearchActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+
+
+    private void addValueEventListener(final DatabaseReference userReference){
+        userReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userList = new ArrayList<>();
+                Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
+                Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
+                while (iterator.hasNext()){
+                    User value = iterator.next().getValue(User.class);
+                    userList.add(value.email);
+                }
+                adapter = new ArrayAdapter<>(
+                        SearchActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        userList);
+                mListView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
