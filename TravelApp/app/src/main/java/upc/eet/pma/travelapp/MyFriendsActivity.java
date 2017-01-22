f4d6dcf86b66cd619cd3cf0e6026ad10211213ff
bertacumellas <bertacume@gmail.com>
2017-01-22 17:34:26 +0100

    MyFriends modificació

--- TravelApp/app/src/main/java/upc/eet/pma/travelapp/MyFriendsActivity.java
+++ TravelApp/app/src/main/java/upc/eet/pma/travelapp/MyFriendsActivity.java
@@ -3,6 +3,7 @@ package upc.eet.pma.travelapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,9 +22,11 @@ public class MyFriendsActivity extends AppCompatActivity {
     private FirebaseDatabase usersDatabase;
     private DatabaseReference usersDatabaseReference;
 
-    private ArrayList<UserChild> userList;
-    private AdapterMyFriends adapter;
+    private ArrayList<String> userList;
+    private ArrayAdapter adapter;
     private ListView mListView;
+
+    private String friend_uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,56 +41,38 @@ public class MyFriendsActivity extends AppCompatActivity {
         final String currentuserId = user.getUid();
         usersDatabaseReference = usersDatabase.getReference().child("Users").child(currentuserId).child("friendsList");
         addValueEventListener(usersDatabaseReference);
+        userList = new ArrayList<>();
+        for ( String key : User.currentUser.friendsList.keySet() ) {
+            friend_uid = User.currentUser.friendsList.get(key).toString();
+            friend_uid = friend_uid.substring(12);
+            friend_uid = friend_uid.substring(0, friend_uid.length()-1);
+            DatabaseReference usersRef = usersDatabase.getReference().child("Users");
+            usersRef.child(friend_uid).child("full_name").addListenerForSingleValueEvent(
+                    new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // Get user value
+                            String full_name = dataSnapshot.getValue(String.class);
+                            //String uLocation = dataSnapshot.getValue(User.class).ulocation;
+                            userList.add(full_name);
+                            adapter = new ArrayAdapter<>(
+                                    MyFriendsActivity.this,
+                                    android.R.layout.simple_list_item_1,
+                                    userList);
+                            mListView.setAdapter(adapter);
+                        }
 
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
 
-        /*
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View item, int pos, long id) {
-                Toast.makeText(MyFriendsActivity.this, String.format("'%s' Profile",
-                        userList.get(pos).getName()), Toast.LENGTH_SHORT).show();
-                //Toast.makeText(SearchActivity.this, String.format("'%s' Profile", userList.get(pos).getDescription()), Toast.LENGTH_SHORT).show();
-
-                Intent i = new Intent("upc.eet.pma.travelapp.FriendActivity");
-                String id_pos = Integer.toString(pos);
-
-                i.putExtra("id_pos", id_pos);
-                i.putExtra("userList", userList);
-                startActivity(i);
-
+                        }
+                    });
 
             }
-        });*/
-
     }
 
-    /*
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
-        MenuItem item = menu.findItem(R.id.menuSearch);
-        SearchView searchView = (SearchView) item.getActionView();
 
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //firebaseListAdapter.notifyDataSetChanged(newText);
-                adapter.getFilter().filter(newText);
-
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    } */
 
 
     private void addValueEventListener(final DatabaseReference userReference) {
@@ -107,31 +92,8 @@ public class MyFriendsActivity extends AppCompatActivity {
                     String S_UidFriend = Uid_friend.toString();
 
                     User.currentUser.friendsList.put("Uid_friend", S_UidFriend);
-                    DatabaseReference usersRef = usersDatabase.getReference().child("Users");
-                    usersRef.child(S_UidFriend).addValueEventListener(
-                            new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    // Get user value
-                                    String full_name = dataSnapshot.getValue(User.class).full_name;
-                                    String uLocation = dataSnapshot.getValue(User.class).ulocation;
-                                    userList.add(new UserChild(full_name,uLocation));
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-
-                                        }
-                            }
-                    );
-
                 }
-                adapter = new AdapterMyFriends(
-                        MyFriendsActivity.this,
-                        userList);
-                mListView.setAdapter(adapter);
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
@@ -139,7 +101,12 @@ public class MyFriendsActivity extends AppCompatActivity {
         });
     }
 
-
-
-
+    public String removeLastCharacter(String str) {
+        if (str != null && str.length() > 0 && str.charAt(str.length()-1)=='x') {
+            str = str.substring(0, str.length()-1);
+        }
+        return str;
     }
+
+
+}
